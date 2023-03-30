46dc20006d95733455421b600122f42580666fcc
parisa bayat <parisa792000b@gmail.com>
2023-03-30 11:41:00 +0430

    Create Notification.php

---
+++ app/Http/Livewire/Home/Profile/Notification.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire\Home\Profile;
+
+use Livewire\Component;
+
+class Notification extends Component
+{
+    public function deleteAllNotification($id)
+    {
+     $notifics =   \App\Models\Notification::where('user_id',$id)
+         ->where('type',1)->get();
+       foreach ($notifics as $not){
+           $not->delete();
+       }
+        $this->emit('toast', 'success', ' پیغام ها با موفقیت حذف شدند.');
+    }
+
+    public function render()
+    {
+        return view('livewire.home.profile.notification')->layout('layouts.home');
+    }
+}
