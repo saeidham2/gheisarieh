68ff03e2cea576408dec2a311717854c3a931025
parisa bayat <parisa792000b@gmail.com>
2023-03-30 11:48:30 +0430

    Create AppServiceProvider.php

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Cart;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ValidatedInput;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Validator;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+//        $this->app->bind('path.public',function (){
+//           return realpath(base_path().'/digikala/public');
+//        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (auth()->user()) {
+            $carts = \App\Models\Cart::where('user_id', auth()->user()->id)->where('type', 0)->get();
+            $userIp2 = Request::ip();
+            $cart2s = \App\Models\Cart::where('ip',$userIp2)->get();
+            if ($cart2s) {
+                foreach ($cart2s as $cart){
+                    $cart->update([
+                        'user_id' =>auth()->user()->id,
+                    ]);
+                }
+
+            }
+        }else
+        {
+            $userIp = Request::ip();
+            $carts = \App\Models\Cart::where('ip', $userIp)->where('type', 0)->get();
+        }
+        View::share('carts',$carts);
+
+
+    Validator::extend('max_mb',function ($attribute,$value,$parameters,$validator){
+    $this->requireParameterCount(1,$parameters,'max_mb');
+    if ($value instanceof UploadedFile && ! $value->isValid()) {
+        return false;
+    }
+    $mb = $value->getSize()/1024/1024;
+    return $this->getSize($attribute, $mb) <= $parameters[0];
+});
+
+    }
+}
