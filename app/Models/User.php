9f40ceeff7d409a8851d49af1e7a6753e37ee4f6
parisa bayat <parisa792000b@gmail.com>
2023-03-30 11:48:16 +0430

    Create User.php

---
+++ app/Models/User.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'img',
+        'mobile',
+        'national_code',
+        'birthday',
+        'job',
+        'money_back',
+        'name_company',
+        'national_code_company',
+        'code_company',
+        'sabt_company',
+        'state_company',
+        'city_company',
+        'phone_company',
+        'wallet',
+        'admin',
+        'staff',
+        'seller',
+        'email_verified_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+//    /**
+//     * The attributes that should be cast to native types.
+//     *
+//     * @var array
+//     */
+//    protected $casts = [
+//        'email_verified_at' => 'datetime',
+//    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    public function isAdmin()
+    {
+        return $this->admin;
+    }
+
+    public function isStaff()
+    {
+        return $this->staff;
+    }
+
+    public function hasPermission($permission)
+    {
+        return $this->permissions->contains('name', $permission->name || $this->hasRole($permission->roles));
+    }
+
+    public function hasRole($roles)
+    {
+        return !! $roles->intersect($this->roles)->all();
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}
