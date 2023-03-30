a137e867a4c808415f3b52e33b68e300e6cb9701
parisa bayat <parisa792000b@gmail.com>
2023-03-30 11:48:33 +0430

    Create AuthServiceProvider.php

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Permission;
+use App\Models\Team;
+use App\Policies\TeamPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use phpDocumentor\Reflection\Types\True_;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Team::class => TeamPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        \Gate::before(function ($user){
+           if ($user->isAdmin()) {
+               return true;
+           }
+        });
+        foreach (Permission::all() as $permission){
+            Gate::define($permission->name,function ($user) use ($permission){
+                return $user->hasPermission($permission);
+            });
+        }
+    }
+}
