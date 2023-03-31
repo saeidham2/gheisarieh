404e2d2d07237ce7c79353c51f783daee704a3c2
parisa bayat <parisa792000b@gmail.com>
2023-03-31 13:47:55 +0430

    Create search.blade.php

---
+++ resources/views/livewire/home/home/setting/header/search.blade.php
@@ -0,0 +1,46 @@
+<div class="c-header__search">
+    <div class="c-search js-search"
+         data-event="using_search_box" data-event-category="header_section">
+        <span
+            class="c-search__reset u-hidden js-search-reset">
+
+                        </span>
+        <input type="text" name="q"
+               placeholder="جستجو در دیجی‌کالا …"
+               class="js-search-input"
+               autocomplete="off"
+               value=""
+        >
+        <div class="c-search__results js-search-results">
+            <ul class="js-autosuggest-results-list c-search__results-list
+             c-search__results-list--autosuggest">
+
+            </ul>
+            <ul class="js-results-list c-search__results-list"></ul>
+            <ul class="js-autosuggest-empty-list c-search__results-list">
+                <li><a class="js-search-keyword-link" href="javascript:">
+                        <span
+                            class="c-search__result-item
+                            c-search__result-icon c-search__result-icon--group">
+                            نمایش همه نتایج برای </span><span
+                            class="c-search__result-item--category js-search-keyword"></span></a></li>
+            </ul>
+            <ul class="c-search__results-list c-search__results-list--history js-last-searches"></ul>
+            <div class="c-search__results-footer">
+                بیشترین جستجوهای اخیر:
+                <ul class="c-search__results-trends js-trends-results"></ul>
+            </div>
+            <div class="c-search__results-list js-search-ad-banner">
+                <div class="c-search__banner"><a
+                        href="product/dkp-3886246/"
+                        title="ظروف پاکیزه با پودر ماشین ظرفشویی پریل"
+                        data-id="60064"
+                        target="_blank" class="js-search-banner-ga">
+                        <img class=""
+                             alt="ظروف پاکیزه با پودر ماشین ظرفشویی پریل"
+                             src="../dkstatics-public.digikala.com"
+                             loading="lazy"/></a></div>
+            </div>
+        </div>
+    </div>
+</div>
