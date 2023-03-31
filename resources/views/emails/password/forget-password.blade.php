e073e3f11da8aca592ee1aa3a80e0e5fc67c4398
parisa bayat <parisa792000b@gmail.com>
2023-03-31 13:24:01 +0430

    Create forget-password.blade.php

---
+++ resources/views/emails/password/forget-password.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ایمیل فراموشی رمز عبور</title>
+</head>
+<body style="direction: rtl;background-color: #0a53be">
+<p style="text-align: center;color: #fff1f3">سلام
+    کد بازیابی رمز عبور شما:</p>
+<p style="text-align: center">{{$code}}</p>
+</body>
+</html>
